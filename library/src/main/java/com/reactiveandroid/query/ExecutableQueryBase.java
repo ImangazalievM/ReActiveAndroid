d3ec940bd370caad6f958fcfa6885a3007f347fa
ImangazalievM <mahach.imangazaliev@mail.ru>
2017-10-29 10:51:42 +0300

    Added executeAsync() for ExecutableQuery

--- library/src/main/java/com/reactiveandroid/query/ExecutableQueryBase.java
+++ library/src/main/java/com/reactiveandroid/query/ExecutableQueryBase.java
@@ -3,6 +3,9 @@ package com.reactiveandroid.query;
 import com.reactiveandroid.Model;
 import com.reactiveandroid.ReActiveAndroid;
 
+import io.reactivex.Completable;
+import io.reactivex.functions.Action;
+
 public abstract class ExecutableQueryBase<T extends Model> extends QueryBase<T> {
 
     public ExecutableQueryBase(Query parent, Class<T> table) {
@@ -11,6 +14,15 @@ public abstract class ExecutableQueryBase<T extends Model> extends QueryBase<T> 
 
     public void execute() {
         ReActiveAndroid.getWritableDatabaseForTable(table).execSQL(getSql(), getArgs());
+    }
+
+    public Completable executeAsync() {
+        return Completable.fromAction(new Action() {
+            @Override
+            public void run() throws Exception {
+                execute();
+            }
+        });
     }
 
 }
